dbdb36efdf0ea814484f6a1c971d1bfe691443d5
bong-suk <greedi9468@hanmail.net>
2024-11-09 18:58:04 +0900

    class형 컴포넌트 및 함수형 컴포넌트 실습

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import "./App.css";
+
+//클래스형 컴포넌트
+//class App extends Component {
+//컴포넌트라는 클래스를 상속받아와서 클래스를 만든다.
+//render() {
+//html형태의 코드를 리턴을 해주는것
+//return <div>안녕하세요!</div>;
+//}
+//}
+
+//함수형 컴포넌트
+function App() {
+  return <div>함수형 컴포넌트입니다!</div>;
+}
+
+export default App;
