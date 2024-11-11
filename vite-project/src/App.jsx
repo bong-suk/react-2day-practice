830f8f1c8e5eded9485bbac762fa6e683c2032b6
bong-suk <greedi9468@hanmail.net>
2024-11-11 10:32:05 +0900

    컴포넌트를 이용한 햄버거 만들기 실습

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,18 +1,74 @@
-import { Component } from "react";
 import "./App.css";
 
-//클래스형 컴포넌트
-//class App extends Component {
-//컴포넌트라는 클래스를 상속받아와서 클래스를 만든다.
-//render() {
-//html형태의 코드를 리턴을 해주는것
-//return <div>안녕하세요!</div>;
-//}
-//}
-
-//함수형 컴포넌트
 function App() {
-  return <div>함수형 컴포넌트입니다!</div>;
+  return (
+    <>
+      <대왕치즈버거 />
+    </>
+  );
+}
+
+function 대왕치즈버거() {
+  return (
+    <>
+      <빵 />
+      <패티 />
+      <양상추 />
+      <토마토 />
+      <치즈 />
+      <빵 />
+    </>
+  );
+}
+
+function 빵() {
+  return (
+    <div className="빵">
+      밀가루
+      <span> 물</span>
+      <span> 계란</span>
+      <span> 이스트</span>
+    </div>
+  );
+}
+
+function 패티() {
+  return (
+    <div className="패티">
+      소고기
+      <span> 후추</span>
+      <span> 소금</span>
+    </div>
+  );
+}
+
+function 양상추() {
+  return (
+    <div className="양상추">
+      잎사귀
+      <span> 줄기</span>
+    </div>
+  );
+}
+
+function 토마토() {
+  return (
+    <div className="양상추">
+      껍질
+      <span> 과육</span>
+      <span> 씨앗</span>
+    </div>
+  );
+}
+
+function 치즈() {
+  return (
+    <div className="치즈">
+      우유
+      <span> 소금</span>
+      <span> 색소</span>
+    </div>
+  );
 }
 
 export default App;
